dc86c243c2853243dcebe37dad3476df07e11a47
langqf <qf.lang@gmail.com>
2016-03-08 22:38:35 +0800

    添加mybatis配置

---
+++ src/main/java/com/whpu/service/stock/spam/pojo/UserTest.java
@@ -0,0 +1,27 @@
+package com.whpu.service.stock.spam.pojo;
+
+public class UserTest {
+	private int id;
+	private String name;
+	private String shuaige;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShuaige() {
+		return shuaige;
+	}
+	public void setShuaige(String shuaige) {
+		this.shuaige = shuaige;
+	}
+	
+	
+}
